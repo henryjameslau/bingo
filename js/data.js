d4b3800af724d00dade95998be44eea0d17748db
henryjameslau <henry.james.lau@gmail.com>
2017-11-10 13:26:28 +0000

    initial commit

---
+++ js/data.js
@@ -0,0 +1,40 @@
+
+var headerText = "2015 Election BBC debate bingo";
+
+var footerText = "Adapted from <a href='https://github.com/jeffehobbs/HTML5-bingo/' target='_blank'>Jeffe Hobbs' HTML5 bingo</a>";
+
+var winText = "You win!";
+
+var clickSnd = new Audio("audio/click.mp3");
+
+var winSnd = new Audio("audio/win.mp3");
+
+var JSONBingo = {"squares": [
+        {"square": "Immigrants"},
+        {"square": "The political class"},
+        {"square": "Open-door"},
+        {"square": "Austerity"},
+        {"square": "Deficit"},
+        {"square": "On May 7th"},
+        {"square": "Hard decisions"},
+        {"square": "Unfunded"},
+        {"square": "Coalition"},
+        {"square": "Our NHS"},
+        {"square": "Strong leader"},
+        {"square": "Love this country"},
+        {"square": "Young People"},
+        {"square": "HIV"},
+        {"square": "Trident"},
+        {"square": "Freeze"},
+        {"square": "A clear choice at this election"},
+        {"square": "Hard working families"},
+        {"square": "Science"},
+        {"square": "Cuts"},
+        {"square": "Food banks"},
+        {"square": "Bedroom tax"},
+        {"square": "Millionaires"},
+        {"square": "Leanne wood speaks Welsh"},    	
+        {"square": "Nicola Sturgeon speaks Ulster-Scots"},    	
+        {"square": "Nigel Farage speaks French"},    	  	
+    ]
+};
